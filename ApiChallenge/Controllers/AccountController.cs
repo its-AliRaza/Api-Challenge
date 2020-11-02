d88dc019825e9794a565add1f711073684a343d1
its-AliRaza <its.aliraza03@gmail.com>
2020-11-02 10:38:15 +0500

    SignUp Method added in Account Controller

--- ApiChallenge/Controllers/AccountController.cs
+++ ApiChallenge/Controllers/AccountController.cs
@@ -30,10 +30,10 @@ namespace ApiChallenge.Controllers
                 var user = new User();
                 if (loginReqest.U_Email != null)
                 {
-                    string query = "SELECT * FROM Patient where P_MNumber= @mobilePhone";
+                    string query = "SELECT * FROM AppUser where U_Email= @userEmail";
                     SqlConnection conn = new SqlConnection(Utils.connStr);
                     SqlCommand command = new SqlCommand(query, conn);
-                    command.Parameters.AddWithValue("@mobilePhone", loginReqest.U_Email);
+                    command.Parameters.AddWithValue("@userEmail", loginReqest.U_Email);
                     conn.Open();
                     SqlDataReader reader = command.ExecuteReader();
                     if (reader.Read())
@@ -71,6 +71,53 @@ namespace ApiChallenge.Controllers
                 return Content(HttpStatusCode.InternalServerError, new Response<string>(ex.StackTrace, ex.Message));
             };
             return Content(HttpStatusCode.InternalServerError, new Response<string>("500", "Something went wrong.Please try again later."));
+        }
+
+        // Post api/Account/Signup
+        [HttpPost]
+        [ResponseType(typeof(string))]
+        [AllowAnonymous]
+        public IHttpActionResult Signup(User userRequest)
+        {
+            try
+            {
+                if (userRequest == null)
+                    return Content(HttpStatusCode.BadRequest, new Response<string>("400", "Request should not be null"));
+                else if (!ModelState.IsValid)
+                    return Content(HttpStatusCode.BadRequest, new Response<string>("400", "This feild is mandatory", null, ModelState));
+
+                string query = "SELECT * FROM AppUser where U_Email= @userEmail";
+                SqlConnection conn = new SqlConnection(Utils.connStr);
+                SqlCommand command = new SqlCommand(query, conn);
+                command.Parameters.AddWithValue("@userEmail", userRequest.U_Email);
+                conn.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    conn.Close();
+                    return Content(HttpStatusCode.BadRequest, new Response<string>("1", "Email already registered, Use another Email."));
+                }
+                else
+                {
+                    userRequest.U_Email = BCrypt.Net.BCrypt.HashPassword(userRequest.U_Password);
+                    using (conn)
+                    {
+                        string insertQuerry = "INSERT INTO AppUser(U_Name,U_Email,U_Password) VALUES(@name,@email,@password)";
+                        using (SqlCommand insertCommand = new SqlCommand(insertQuerry, conn))
+                        {
+                            insertCommand.Parameters.AddWithValue("@name", userRequest.U_Name);
+                            insertCommand.Parameters.AddWithValue("@email", userRequest.U_Email);
+                            insertCommand.Parameters.AddWithValue("@password", userRequest.U_Password);
+                            insertCommand.ExecuteNonQuery();
+                        }
+                    }
+                    conn.Close();
+                    return Content(HttpStatusCode.OK, new Response<string>("200", "Signup created successfully."));
+                }
+            }
+            catch { }
+            return Content(HttpStatusCode.InternalServerError, new Response<string>("500", "Something went wrong.Please try again later.")); ;
+
         }
     }
 }
