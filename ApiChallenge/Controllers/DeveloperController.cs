3872082e1a87ff244da9dc5f6cb0ebec31b25aeb
its-AliRaza <its.aliraza03@gmail.com>
2020-11-02 11:08:52 +0500

    Get and Add Developers methods added in controller. Also filters are added for JWT Authentication.

---
+++ ApiChallenge/Controllers/DeveloperController.cs
@@ -0,0 +1,116 @@
+﻿
+using ApiChallenge.Filters;
+using ApiChallenge.Helpers;
+using ApiChallenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.Mvc;
+using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
+
+namespace ApiChallenge.Controllers
+{
+    public class DeveloperController : ApiController
+    {
+        // Post api/Developer/GetDevelopers
+        [HttpPost]
+        [ResponseType(typeof(TotalDeveloper))]
+        [JwtAuthentication]
+        public IHttpActionResult GetDevelopers(DeveloperRequest developerRequest)
+        {
+            try
+            {
+                if (developerRequest == null)
+                    return Content(HttpStatusCode.BadRequest, new Response<string>("400", "Mobile Phone is mandatory."));
+                else if (!ModelState.IsValid)
+                    return Content(HttpStatusCode.BadRequest, new Response<string>("400", "This feild is mandatory", null, ModelState));
+                var profile = JwtManager.GetProfile(Request.Headers.Authorization.Parameter);
+                string query = "SELECT * FROM Developer where D_CreatedBy= @emial and D_CreatedAt >= @FromDate and D_CreatedAt <= @ToDate ORDER BY D_ID DESC OFFSET " + (developerRequest.Page - 1) * 5 + " ROWS FETCH NEXT 5 ROW ONLY";
+                SqlConnection conn = new SqlConnection(Utils.connStr);
+                conn.Open();
+
+                SqlCommand command = new SqlCommand(query, conn);
+                command.Parameters.AddWithValue("@email", profile.U_Email);
+                command.Parameters.AddWithValue("@FromDate", developerRequest.From);
+                command.Parameters.AddWithValue("@ToDate", developerRequest.To);
+                TotalDeveloper result = new TotalDeveloper();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Developer item = new Developer()
+                        {
+                            D_ID = (int)reader["D_ID"],
+                            D_Email = reader["D_Email"].ToString(),
+                            D_Name = reader["D_Name"].ToString(),
+                            D_Age = reader["D_Age"].ToString(),
+                            D_CreatedBy = reader["D_CreatedBy"].ToString(),
+                            D_CreatedAt = Convert.ToDateTime(reader["D_CreatedAt"]).ToString("dd-MMM-yyy")
+                        };
+                        result.Reports.Add(item);
+                    }
+                }
+                command.Parameters.Clear();
+                string selectQuerry = "SELECT COUNT(1) FROM Developer where D_CreatedBy= @emial and D_CreatedAt >= @FromDate and D_CreatedAt <= @ToDate";
+                SqlCommand selectCommand = new SqlCommand(selectQuerry, conn);
+                selectCommand.Parameters.AddWithValue("@emial", profile.U_Email);
+                selectCommand.Parameters.AddWithValue("@FromDate", developerRequest.From);
+                selectCommand.Parameters.AddWithValue("@ToDate", developerRequest.To);
+
+
+                Int32 count = (Int32)selectCommand.ExecuteScalar();
+                int pages = (int)Math.Ceiling(count / 5.0);
+                result.TotalPages = pages;
+                conn.Close();
+                return Content(HttpStatusCode.OK, new Response<TotalDeveloper>("200", "Developers Fetch Successfully.", result)); ;
+            }
+            catch (Exception ex) { }
+            return Content(HttpStatusCode.InternalServerError, new Response<string>("500", "Something went wrong.Please try again later.")); ;
+        }
+
+        // Post api/Developer/AddDevelopers
+        [HttpPost]
+        [ResponseType(typeof(string))]
+        [JwtAuthentication]
+        public IHttpActionResult AddDeveloper(Developer developer)
+        {
+            try
+            {
+                if (developer == null)
+                    return Content(HttpStatusCode.BadRequest, new Response<string>("400", "Request should not be null"));
+                else if (!ModelState.IsValid)
+                    return Content(HttpStatusCode.BadRequest, new Response<string>("400", "This feild is mandatory", null, ModelState));
+
+                var profile = JwtManager.GetProfile(Request.Headers.Authorization.Parameter);
+                SqlConnection conn = new SqlConnection(Utils.connStr);
+                conn.Open();
+                using (conn)
+                {
+                    string insertQuerry = "INSERT INTO Developer(D_Name,D_Email,D_Age,D_CreatedBy,D_CreatedAt,U_ID) VALUES(@dname,@demail,@dage,@dCreatedBy,@dCreatedAt,@uID)";
+                    using (SqlCommand insertCommand = new SqlCommand(insertQuerry, conn))
+                    {
+                        insertCommand.Parameters.AddWithValue("@dname", developer.D_Name);
+                        insertCommand.Parameters.AddWithValue("@demail", developer.D_Email);
+                        insertCommand.Parameters.AddWithValue("@dage", developer.D_Age);
+                        insertCommand.Parameters.AddWithValue("@dCreatedBy", developer.D_CreatedBy);
+                        insertCommand.Parameters.AddWithValue("@dCreatedAt", developer.D_CreatedAt);
+                        insertCommand.Parameters.AddWithValue("@uID", profile.U_ID);
+                        insertCommand.ExecuteNonQuery();
+                    }
+                }
+                conn.Close();
+                return Content(HttpStatusCode.OK, new Response<string>("200", "Developer Added successfully."));
+            }
+            catch (Exception ex) { }
+            return Content(HttpStatusCode.InternalServerError, new Response<string>("500", "Something went wrong.Please try again later.")); ;
+
+        }
+    }
+}
---
+++ ApiChallenge/Models/Developer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ApiChallenge.Models
+{
+    public class Developer
+    {
+        public int D_ID { get; set; }
+
+        public string U_ID { get; set; }
+
+        [Required(ErrorMessage = "Developer Name is required.")]
+        [Display(Name = "Developer Name")]
+        public string D_Name { get; set; }
+
+        [Required(ErrorMessage = "Developer Email is required.")]
+        [Display(Name = "Developer Email")]
+        public string D_Email { get; set; }
+
+        [Required(ErrorMessage = "User Email is required.")]
+        [Display(Name = "User Email")]
+        public string U_Email { get; set; }
+
+        [Required(ErrorMessage = "Age is required.")]
+        [Display(Name = "Age")]
+        public string D_Age { get; set; }
+
+        public string D_CreatedAt { get; set; }
+
+        public string D_CreatedBy { get; set; }
+    }
+
+    public class TotalDeveloper
+    {
+        public List<Developer> Reports { get; set; } = new List<Developer>();
+        public int TotalPages { get; set; }
+    }
+
+    public class DeveloperRequest
+    {
+        [Required(ErrorMessage = "From  Date is required.")]
+        [Display(Name = "From Date")]
+        public string From { get; set; }
+        [Required(ErrorMessage = "To  Date is required.")]
+        [Display(Name = "To Date")]
+        public string To { get; set; }
+        public int Page { get; set; } = 1;
+    }
+}
